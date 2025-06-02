d48431eed927f9197e248032c28adef01c396f6f
adhishwaran <aadhishwaran34@gmail.com>
2025-06-02 11:19:25 +0530

    c++

--- basics/calc.cpp
+++ basics/calc.cpp
@@ -3,6 +3,39 @@ using namespace std;
 
 int main(){
     int a;
+    cout<<"enter a: ";
+    cin>>a;
     int b;
-    
+    cout<<"enter b: ";
+    cin>>b;
+    char op;
+    cout<<"chose an operator (+,-,*,/)";
+    cin>>op;
+    switch (op){
+        case '+':{
+            int c = a+b;
+            cout<<c;
+            break;
+        }
+        case '-':{
+            int c = a-b;
+            cout<<c;
+            break;
+        }
+        case '*':{
+            int c = a*b;
+            cout<<c;
+            break;
+        }
+        case '/':{
+            int c = a/b;
+            cout<<c;
+            break;
+        }
+        default:{
+            int c = a+b;
+            cout<<c;
+        }
+    }
+    return 1;
 }
--- basics/r_w.cpp
+++ basics/r_w.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     string name;
     cout<<"enter the name ";
     cin>>name;
