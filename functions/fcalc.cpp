dbae533dca46945b560eaad2d0cc1bfbd3dcc958
adhishwaran <aadhishwaran34@gmail.com>
2025-06-02 11:43:16 +0530

    fcalc

--- functions/fcalc.cpp
+++ functions/fcalc.cpp
@@ -1,1 +1,59 @@
 #include<iostream>
+using namespace std;
+
+int add(){
+    int a,b;
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    int c = a+b;
+    cout<<c;
+    return 1;
+}
+
+int sub(){
+    int a,b;
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    int c = a-b;
+    cout<<c;
+    return 1;
+}
+
+int mul(){
+    int a,b;
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    int c = a*b;
+    cout<<c;
+    return 1;
+}
+
+int div(){
+    int a,b;
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    int c = a/b;
+    cout<<c;
+    return 1;
+}
+
+int main(){
+    char x;
+    cout<<"enter op(+,-,*,/): ";
+    cin>>x;
+    switch (x) {
+        case '+':{add();}break;
+        case '-':{sub();}break;
+        case '*':{mul();}break;
+        case '/':{div();}break;
+    }
+    return 1;
+}
