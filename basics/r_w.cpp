f4a893197896750b318625dd8c296a5ba79e6628
adhishwaran <aadhishwaran34@gmail.com>
2025-06-02 10:57:13 +0530

    Initial commit

---
+++ basics/r_w.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"enter the name ";
+    cin>>name;
+    int age;
+    cout<<"enter the age ";
+    cin>>age;
+    int tenth;
+    cout<<"enter the 10th mark ";
+    cin>>tenth;
+    int tewl;
+    cout<<"enter the 12th mark ";
+    cin>>tewl;
+    cout<<"Name is "<<name<<" age is"<<age<<" 10th mark is"<<tenth<<" 12th mark is"<<tewl;
+    return 1;
+}
