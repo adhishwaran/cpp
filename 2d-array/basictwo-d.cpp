b106dc2942fed13cede06fbc062112ea30f2885d
adhishwaran <aadhishwaran34@gmail.com>
2025-06-03 10:46:42 +0530

    ohhhhhh

---
+++ 2d-array/basictwo-d.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[2][2] = {{1,3},{2,3}};
+    cout<<arr;
+}
---
+++ fun-ovl/display.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int display(string name,int ,float ,bool ){}
+
+int main(){
+    string name;
+    cout<<"enter name: ";
+    cin>>name;
+    return 1;
+}
