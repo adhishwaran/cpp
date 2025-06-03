4c926d093112cd31403cec09e5fc600c9f25bfee
adhishwaran <aadhishwaran34@gmail.com>
2025-06-03 11:06:51 +0530

    noooo

--- fun-ovl/display.cpp
+++ fun-ovl/display.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int display(string name,int ,float ,bool ){}
-
 int main(){
-    string name;
-    cout<<"enter name: ";
-    cin>>name;
-    int age;
-    cout<<"enter age: ";
-    cin>>age;
-    float marks;
-    cout<<"enter marks: ";
-    cin>>marks;
-    bool result;
-    cout<<"enter result";
-    cin>>result;
-    display(name,age,marks,result);
+    
     return 1;
 }
