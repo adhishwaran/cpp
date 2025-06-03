8af4eb6b5fd78883c944d13e8872d570761efaba
adhishwaran <aadhishwaran34@gmail.com>
2025-06-03 11:00:30 +0530

    nn

---
+++ basics/bool.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool a;
+    cout<<"enter a: ";
+    cin>>a;
+    if (a==1)cout<<"true";
+    else cout<<"false";
+    return 0;
+}
--- fun-ovl/display.cpp
+++ fun-ovl/display.cpp
@@ -7,5 +7,15 @@ int main(){
     string name;
     cout<<"enter name: ";
     cin>>name;
+    int age;
+    cout<<"enter age: ";
+    cin>>age;
+    float marks;
+    cout<<"enter marks: ";
+    cin>>marks;
+    bool result;
+    cout<<"enter result";
+    cin>>result;
+    display(name,age,marks,result);
     return 1;
 }
