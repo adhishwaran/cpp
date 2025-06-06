b25d6f3e71df1baa9c837d47bcfea65bf44b72bf
adhishwaran <aadhishwaran34@gmail.com>
2025-06-06 17:07:26 +0530

    n

--- fun-ovl/power.cpp
+++ fun-ovl/power.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 using namespace std;
 
-inline int multi(int n,int m){
-    if (m==0) return 1;
-    return n * multi(n,m-1);
+int multi(int bas,int exp){
+    if (exp==0) return 1;
+    return bas * multi(bas,exp-1);
+}
+
+// int multi(float base,int exp){
+//     if (base==0) return 1;
+//     return exp * multi(base,exp-1);
+// }
+
+// int multi(int bas,float expo){
+//     if (bas==0) return 1;
+//     return expo * multi(bas,expo-1);
+// }
+
+int multi(float base,float expo){
+    if (base==0) return 1;
+    return expo * multi(base,expo-1);
 }
 
 int main(){
-    int n,m;
-    cout<<"enter n: ";
-    cin>>n;
-    cout<<"enter m: ";
-    cin>>m;
-    if (n==m){cout<<multi(n,n);}
-    else {cout<<multi(n,m);}
+    int bas,exp;
+    cout<<"enter bas: ";
+    cin>>bas;
+    cout<<"enter exp: ";
+    cin>>exp;
+    float base,expo;
+    cout<<"enter base: ";
+    cin>>base;
+    cout<<"enter expo: ";
+    cin>>expo;
+    int x = multi(bas,exp);
+    cout<<"the ans is: "<<x;
+    int y = multi(base,expo);
+    cout<<"the ans is: "<<y;
     return 1;
 }
