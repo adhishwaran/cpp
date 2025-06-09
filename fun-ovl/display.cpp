862eaecd2f4c7147762fb1c51f48e737d0aaf4f6
adhishwaran <aadhishwaran34@gmail.com>
2025-06-09 11:52:20 +0530

    one

--- fun-ovl/display.cpp
+++ fun-ovl/display.cpp
@@ -1,3 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int display(string name){
+    string name;
+    cout<<"enter name: ";
+    cin>>name;
+    cout<<name;
+} 
+
+int age(){
+    int age;
+    cout <<"enter age: ";
+    cin>>age;
+    cout <<age;
+}
+
+int main(){
+
+}
