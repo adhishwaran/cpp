d7b279f42f0b52833a72d5eeb5056e602168af76
adhishwaran <aadhishwaran34@gmail.com>
2025-06-10 11:44:18 +0530

    rectangle

--- class/area.cpp
+++ class/area.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 class Area{
     public:
-    
+    string side,length,breath,hight,base,radius;
+    Area(){
+        cout<<"enter side";
+        cin>>side;
+        
+    }
 };
