0033c36f462594c56634f67a5ecff2d6154bad30
adhishwaran <aadhishwaran34@gmail.com>
2025-06-10 11:20:20 +0530

    psudovolcanosis

---
+++ class/array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Assignment{
+    public:
+    int arr[5];
+    void Assign(){
+        cout<<"enter: ";
+        for (int i=0;i<5;);
+    }
+};
--- class/expri.cpp
+++ class/expri.cpp
@@ -5,31 +5,27 @@ class Student{
     public:
     string name;
     int age;
-    int father;
-    int mother;
+    string father;
+    string mother;
     int sibling;
     void stud(){
-        string name;
         cout<<"enter your name: ";
         cin>>name;
-        int age ;
         cout<< "enter stu age: ";
         cin>>age;
-        int father , mother;
         cout<<"enter your father name: ";
         cin>>father;
         cout<<"enter your mother name: ";
         cin>>mother;
-        int sibling;
         cout<<"enter no of sibling: ";
         cin>>sibling;
     }
     int display(){
-        cout<<name;
-        cout<<age;
-        cout<<father;
-        cout<<mother;
-        cout<<sibling;
+        cout<<name<<endl;
+        cout<<age<<endl;
+        cout<<father<<endl;
+        cout<<mother<<endl;
+        cout<<sibling<<endl;
         return 0;
     }
 };
@@ -37,6 +33,6 @@ class Student{
 int main(){
     Student s1;
     s1.stud();
-    
+    s1.display();
     return 0;
 }
