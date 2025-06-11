3da0df03c7d6af43702e9c3285594afab8956910
adhishwaran <aadhishwaran34@gmail.com>
2025-06-11 10:47:13 +0530

    fuuufuuufuufufufuufufufu

--- class/array.cpp
+++ class/array.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 class Assignment{
     public:
-    int arr[5];
+    int n;
+    int arr[100];
     void Assign(){
+        cout<<"enter n: ";
+        cin>>n;
         cout<<"enter: ";
-        for (int i=0;i<5;);
+        for (int i=0;i<n;i++){
+            cout<<"enter: ";
+            cin>>arr[i];
+        };
+    }
+    int avg(){
+        int tot = 0;
+        for (int i = 0;i<n;i++){
+            tot += arr[i];
+        }
+        int avg = tot/n;
+        return avg;
     }
 };
+
+int main(){
+    Assignment s1;
+    s1.Assign();
+    cout<<"Average is: "<<s1.avg();
+    return 0;
+}
