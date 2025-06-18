544b06b8cbd1850ca6115ed6787ca90871305af0
adhishwaran <aadhishwaran34@gmail.com>
2025-06-18 10:54:38 +0530

    up

--- class/area.cpp
+++ class/area.cpp
@@ -5,29 +5,6 @@ class Areaofshapes{
     public:
     int side,length,breath,hight,base,radius;
     void Area(){
-        int x;
-        cout<<"enter x: ";
-        cin>>x;
-        switch(x){
-            case 1:{
-                int c = side*side;
-                cout<<"The area of square is : "<<c;
-            }
-            case 2:{
-                int c = length * breath;
-                cout<<"the area of rectangle is : "<<c;
-            }
-            case 3:{
-                float s = 3.14*(radius*radius);
-                cout<<"the area of circle is : "<<s;
-            }
-            case 4 :{
-                float t = 0.5*base*hight;
-                cout<<"the area of triangle is : "<<t;
-            }
-        }       
-    }
-    int cnons(){
         cout<<"enter side: ";
         cin>>side;
         cout<<"enter length: ";
@@ -40,13 +17,43 @@ class Areaofshapes{
         cin>>base;
         cout<<"enter radius: ";
         cin>>radius;
-        return 0;
+    }
+    int constan(){
+        int x;
+        cout<<"enter x: ";
+        cin>>x;
+        switch(x){
+            case 1:{
+                int c = side*side;
+                cout<<"The area of square is : "<<c<<endl;
+                return 1;
+            }
+            case 2:{
+                int c = length * breath;
+                cout<<"the area of rectangle is : "<<c<<endl;
+                return 1;
+            }
+            case 3:{
+                float s = 3.14*(radius*radius);
+                cout<<"the area of circle is : "<<s<<endl;
+                return 1;
+            }
+            case 4 :{
+                float t = 0.5*base*hight;
+                cout<<"the area of triangle is : "<<t<<endl;
+                return 1;
+            }
+            default: {
+                cout << "Invalid option selected!" << endl;
+                return 0;
+            }
+        }  
     }
 };
 
 int main(){
     Areaofshapes s1;
     s1.Area();
-    s1.cnons();
+    s1.constan();
     return 0;
 }
