33a1a78a08d64779cb5bd14fb94635b3907c0e58
adhishwaran <aadhishwaran34@gmail.com>
2025-06-18 11:30:29 +0530

    offfffff

--- class/recurrsion/factorial.cpp
+++ class/recurrsion/factorial.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Factoial{
-    
+class Factorial{
+    public :
+    int number;
+    void Facto(){
+        cout<<"enter number :  ";
+        cin>>number;
+    }
+    int calculation(int number){
+         if (number == 0) return 1;
+         if (number == 1)  return 1;
+         return number * calculation(number - 1); 
+    }
+    void Result(){
+        cout << "Factorial of " << number << " is " << calculation(number) << endl;
+    }
 };
+
+int main(){
+    Factorial f;
+    f.Facto();
+    f.Result();
+    return 0;
+}
