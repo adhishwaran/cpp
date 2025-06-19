111afcfe696fcca7f8fe6a96403ab9be9f16ae9d
adhishwaran <aadhishwaran34@gmail.com>
2025-06-19 10:24:36 +0530

    op

--- class/stru.cpp
+++ class/stru.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+struct Address{
+    int dr_no;
+    string name;
+    string street;
+    string city;
+    string district;
+};
+
+class Information{
+    public :
+    string s_name;
+    int age;
+    struct Address address1;
+    void input(){
+        cout<<"enter name: ";
+        cin>>s_name;
+        cout<<"enter age: ";
+        cin>>age;
+        cout<<"enter address: ";
+        cin>>address1;
+   }
+};
