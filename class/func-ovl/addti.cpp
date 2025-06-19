bc702895ba2481790c3332299a59d642d46d13be
adhishwaran <aadhishwaran34@gmail.com>
2025-06-19 11:44:01 +0530

    o

--- class/func-ovl/addti.cpp
+++ class/func-ovl/addti.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 class Addtion{
     public:
-    
+    int a,b,c;
+    float a1,b1,c1;
+    void input(){
+        cout<<"enter a: ";
+        cin>>a;
+        cout<<"enter b:";
+        cin>>b;
+        cout<<"enter c:";
+        cin>>c;
+        cout<<"enter a1: ";
+        cin>>a1;
+        cout<<"enter b1:";
+        cin>>b1;
+        cout<<"enter c1:";
+        cin>>c1;
+    }
 };
